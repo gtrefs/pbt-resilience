a70ac6af583388ff35e13dd0459a293828a023e7
Gregor Trefs <Gregor.Trefs@gmail.com>
2019-08-18 00:09:49 +0200

    Add withPrecondition combinator
    Use preconditions to indicate if an action is applicable to container, or not.

--- src/test/java/de/gtrefs/pbtc/SingleDockerContainer.java
+++ src/test/java/de/gtrefs/pbtc/SingleDockerContainer.java
@@ -9,8 +9,10 @@ import org.testcontainers.images.builder.ImageFromDockerfile;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.function.Predicate;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 public class SingleDockerContainer {
     final static Path RESOURCE_PATH = Paths.get("src/test/resources/docker/running-alpine");
@@ -40,6 +42,25 @@ public class SingleDockerContainer {
         }
     }
 
+    @Property(tries = 10)
+    public void an_action_with_precondition_false_should_not_be_executed(@ForAll("precondtionAlwaysFalse")ActionSequence<GenericContainer> actions){
+        assertThatCode(() -> {
+            GenericContainer noContainer = null;
+            GenericContainer after = actions.run(noContainer);
+        }).isInstanceOf(JqwikException.class).hasMessage("Could not generated a single action. At least 1 is required.");
+        assertThat(actions.runActions()).hasSize(0);
+    }
+
+    @Property(tries = 10)
+    public void a_start_action_should_not_be_run_if_the_container_is_running(@ForAll("noStartsWhenContainerIsRunning") ActionSequence<GenericContainer> actions){
+        try(GenericContainer alpine = new GenericContainer(RUNNING_ALPINE_IMAGE)) {
+            actions.run(alpine);
+            assertThat(alpine.isRunning()).isEqualTo(true);
+            assertThat(actions.runActions()).hasSize(1);
+        }
+    }
+
+
     @Provide
     Arbitrary<ActionSequence<GenericContainer>> startAndStops(){
         return Arbitraries.sequences(Arbitraries.of(DockerAction.start(), DockerAction.stop()))
@@ -50,6 +71,20 @@ public class SingleDockerContainer {
     @Provide
     Arbitrary<ActionSequence<GenericContainer>> starts(){
         return Arbitraries.sequences(Arbitraries.of(DockerAction.start()))
+                .ofMinSize(3)
+                .ofMaxSize(10);
+    }
+
+    @Provide
+    Arbitrary<ActionSequence<GenericContainer>> precondtionAlwaysFalse(){
+        return Arbitraries.sequences(Arbitraries.of(DockerAction.start().withPrecondition(container -> false)))
+                .ofMinSize(3)
+                .ofMaxSize(10);
+    }
+
+    @Provide
+    Arbitrary<ActionSequence<GenericContainer>> noStartsWhenContainerIsRunning(){
+        return Arbitraries.sequences(Arbitraries.of(DockerAction.start().withPrecondition(container -> !container.isRunning())))
                 .ofMinSize(3)
                 .ofMaxSize(10);
     }
@@ -66,6 +101,21 @@ public class SingleDockerContainer {
             return container -> {
                 container.stop();
                 return container;
+            };
+        }
+
+        default DockerAction withPrecondition(Predicate<GenericContainer> precondition){
+            var delegate = this;
+            return new DockerAction() {
+                @Override
+                public boolean precondition(GenericContainer model) {
+                    return precondition.test(model);
+                }
+
+                @Override
+                public GenericContainer run(GenericContainer model) {
+                    return delegate.run(model);
+                }
             };
         }
     }
