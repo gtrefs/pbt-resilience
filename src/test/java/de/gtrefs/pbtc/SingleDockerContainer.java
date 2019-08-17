ceb6a8d8195aa3a680134d27741d33bf32087d15
Gregor Trefs <Gregor.Trefs@gmail.com>
2019-08-18 00:08:04 +0200

    Use custom alpine image
    The current alpine image immediately quits after start up. The new image runs an infinite bash loop.

--- src/test/java/de/gtrefs/pbtc/SingleDockerContainer.java
+++ src/test/java/de/gtrefs/pbtc/SingleDockerContainer.java
@@ -4,24 +4,29 @@ import net.jqwik.api.*;
 import net.jqwik.api.stateful.Action;
 import net.jqwik.api.stateful.ActionSequence;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.images.builder.ImageFromDockerfile;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SingleDockerContainer {
+    final static Path RESOURCE_PATH = Paths.get("src/test/resources/docker/running-alpine");
+    ImageFromDockerfile RUNNING_ALPINE_IMAGE = new ImageFromDockerfile().withFileFromPath(".", RESOURCE_PATH);
 
     @Property(tries = 10)
     public void starting_a_container_should_be_idempotent(@ForAll("starts") ActionSequence<GenericContainer> starts){
-        try(GenericContainer alpine = new GenericContainer()) {
+        try(GenericContainer alpine = new GenericContainer(RUNNING_ALPINE_IMAGE)) {
             starts.run(alpine);
             assertThat(alpine.isRunning()).isEqualTo(true);
         }
     }
 
-    @Property(tries = 60)
+    @Property(tries = 10)
     public void a_container_should_always_be_in_the_last_action_state(@ForAll("startAndStops") ActionSequence<GenericContainer> startsAndStops){
-        try(GenericContainer alpine = new GenericContainer()) {
+        try(GenericContainer alpine = new GenericContainer(RUNNING_ALPINE_IMAGE)) {
             startsAndStops.run(alpine);
             List<Action<GenericContainer>> actions = startsAndStops.runActions();
             if(actions.get(actions.size() -1).equals(DockerAction.stop())){
